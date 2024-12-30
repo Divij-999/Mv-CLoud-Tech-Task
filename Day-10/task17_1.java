8e156b9d1726bb11c35980885f0149e174fb14b3
Divij-999 <22002170410010@ljku.edu.in>
2024-12-31 00:51:06 +0530

    Day 10 task 17 completed

--- Day-10/task17_1.java
+++ Day-10/task17_1.java
@@ -3,7 +3,7 @@ public class task17_1{
         int n = 4;
         for(int i = 1 ; i<=n ; i++){
             for(int j = 1 ; j<=i ; j++){
-                System.out.print(j);
+                System.out.print((i*i-i+2)/2+(j-1));
             }
             System.out.println();
         }
